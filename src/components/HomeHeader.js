008d9c3ad9942ba85f8bb37f0d2704b380a7b90c
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-17 21:48:24 +1200

    Added Basic Structure of Header

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,36 @@
+import styles from "./styles/HomeHeader.module.css";
+import logo from "./images/Logo.png";
+import nzFlag from "./images/NZFlag.png";
+import maoriFlag from "./images/MaoriFlag.png";
+import userCircle from "./images/UserCircle.png";
+
+export default function HomeHeader() {
+  return (
+    <div id={styles.header}>
+      <div id={styles.logo}>
+        <img src={logo} alt="LevelUp Works Logo" />
+      </div>
+      <div id={styles.headerNav}>
+        <div>HOME</div>
+        <div>FEATURES</div>
+        <div>TEACHERS</div>
+      </div>
+      <div id={styles.loginLanguage}>
+        <div id={styles.holder}>
+          <div id={styles.language}>
+            LANG <img className={styles.flag} src={nzFlag} alt="NZ" />{" "}
+            <img className={styles.flag} src={maoriFlag} alt="Maori" />
+          </div>
+          <div id={styles.login}>
+            <img
+              style={{ padding: "0px 5px" }}
+              src={userCircle}
+              alt="User Icon"
+            />
+            <div>REGISTER | LOGIN</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import HomeHeader from "../components/HomeHeader"
 import Footer from "../components/shared/Footer"
 
 export default function Home() {
     return (
-        <Footer/>
-    )
+      <div>
+        <HomeHeader />
+        <Footer />
+      </div>
+    );
 }
