024ae956df82b93193bd6b1def9fb30a7953711c
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-27 20:41:36 +1200

    Added functional 4th section to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Footer from "../components/shared/Footer"
 import Intro from "../components/home/Intro"
 import HomeSection2 from "../components/home/HomeSection2";
 import HomeSection3 from "../components/home/HomeSection3";
+import HomeSection4 from "../components/home/HomeSection4";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Intro />
         <HomeSection2 />
         <HomeSection3 />
+        <HomeSection4 />
         <Footer />
       </div>
     );
