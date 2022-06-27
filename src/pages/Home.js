16249aee1f39f39efa83f401ac64fea2e1aebec6
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-27 20:38:50 +1200

    Added third section to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import HomeHeader from "../components/home/HomeHeader"
 import Footer from "../components/shared/Footer"
 import Intro from "../components/home/Intro"
 import HomeSection2 from "../components/home/HomeSection2";
+import HomeSection3 from "../components/home/HomeSection3";
 
 export default function Home() {
     return (
       <div>
         <HomeHeader />
         <Intro />
-        <HomeSection2/>
+        <HomeSection2 />
+        <HomeSection3 />
         <Footer />
       </div>
     );
