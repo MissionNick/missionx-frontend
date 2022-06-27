60a0b2a57a68ea4fef4d90fd506f4934a96db07b
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-27 20:44:31 +1200

    Added last section to home page. Layout Completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Intro from "../components/home/Intro"
 import HomeSection2 from "../components/home/HomeSection2";
 import HomeSection3 from "../components/home/HomeSection3";
 import HomeSection4 from "../components/home/HomeSection4";
+import HomeSection5 from "../components/home/HomeSection5";
 
 export default function Home() {
     return (
@@ -13,6 +14,7 @@ export default function Home() {
         <HomeSection2 />
         <HomeSection3 />
         <HomeSection4 />
+        <HomeSection5 />
         <Footer />
       </div>
     );
