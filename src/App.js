8ae7d75e2e01c4dc008e06fce446af975f79faa1
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-28 22:35:47 +1200

    forgot small change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Home from "./pages/Home";
-=======
-import MockHome from './pages/MockHome'
->>>>>>> main
 import StudentProjectLibrary from "./pages/StudentProjectLibrary";
 import MockTeacher from './pages/MockTeacher'
 
@@ -12,11 +8,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<Home />} />
-=======
-          <Route path="/" element={<MockHome />} />
->>>>>>> main
           <Route path="/StudentProjectLibrary" element={<StudentProjectLibrary />} />
           <Route path="/MockTeacher" element ={<MockTeacher />} />
         </Routes>
