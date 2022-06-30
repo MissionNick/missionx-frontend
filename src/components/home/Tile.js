dca51dbb03f10d3acfb04ace33349fd8e0db6161
perstraine <41206120+perstraine@users.noreply.github.com>
2022-06-30 16:53:56 +1200

    Made Home section 2 functional

--- src/components/home/Tile.js
+++ src/components/home/Tile.js
@@ -1,8 +1,8 @@
 import styles from "../styles/home/HomeSection2.module.css";
 
-export default function Tile({ image, tileName }) {
+export default function Tile({ image, tileName , projectButtonClick, pos}) {
   return (
-    <div id={styles.tileHolder}>
+    <div id={styles.tileHolder} onClick={(e)=>projectButtonClick(e,pos)}>
       <img id={styles.tileImage} src={image} alt="Frame" />
       <div id={styles.tileName}>{tileName}</div>
     </div>
