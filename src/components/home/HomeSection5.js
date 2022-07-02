2ed2b3a3fc9e449d93ff4c871628ffacb7fe4a12
perstraine <41206120+perstraine@users.noreply.github.com>
2022-07-02 13:29:35 +1200

    Home Section 5 made responsive

--- src/components/home/HomeSection5.js
+++ src/components/home/HomeSection5.js
@@ -8,24 +8,26 @@ export default function HomeSection5() {
         <img id={styles.teacherimg} src={teacher} alt="teacher" />
       </div>
       <div id={styles.sectionFiveRight}>
-        <div id={styles.sectionFiveTitle}>What are you waiting for?</div>
-        <div id={styles.sectionFiveSubtitle}>
-          Start teaching Digital Technologies today.
-        </div>
-        <div id={styles.sectionFiveInfo}>
-          If you need more information, we are happy to answer any questions you
-          may have.
-        </div>
-        <div id={styles.sectionFiveButtonHolder}>
-          <button className={styles.sectionFiveButtons} id={styles.enquire}>
-            ENQUIRE NOW
-          </button>
-          <button
-            className={styles.sectionFiveButtons}
-            id={styles.sectionFiveSignup}
-          >
-            SIGN UP
-          </button>
+        <div id={styles.sectionFiveRightWrapping}>
+          <div id={styles.sectionFiveTitle}>What are you waiting for?</div>
+          <div id={styles.sectionFiveSubtitle}>
+            Start teaching Digital Technologies today.
+          </div>
+          <div id={styles.sectionFiveInfo}>
+            If you need more information, we are happy to answer any questions
+            you may have.
+          </div>
+          <div id={styles.sectionFiveButtonHolder}>
+            <button className={styles.sectionFiveButtons} id={styles.enquire}>
+              ENQUIRE NOW
+            </button>
+            <button
+              className={styles.sectionFiveButtons}
+              id={styles.sectionFiveSignup}
+            >
+              SIGN UP
+            </button>
+          </div>
         </div>
       </div>
     </div>
