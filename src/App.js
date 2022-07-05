b8f605213a78afcc364e440d68261f727be21d63
Brandon Trump <brandontrump101@gmail.com>
2022-07-05 20:33:38 +1200

    added routes for sidebar pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Home from "./pages/Home";
 import StudentProjectLibrary from "./pages/StudentProjectLibrary";
 import MockTeacher from "./pages/MockTeacher";
 import StudentProfiles from "./pages/StudentProfiles";
+import ProgressTracker from "./pages/ProgressTracker";
+import HelpRequests from "./pages/HelpRequests";
+import ProjectSubmissions from "./pages/ProjectSubmissions";
+import Profile from "./pages/Profile";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -15,7 +20,12 @@ function App() {
             element={<StudentProjectLibrary />}
           />
           <Route path="/MockTeacher" element={<MockTeacher />} />
+          <Route path="/progress-tracker" element={<ProgressTracker />} />
           <Route path="/student-profiles" element={<StudentProfiles />} />
+          <Route path="/help-requests" element={<HelpRequests />} />
+          <Route path="/project-submissions" element={<ProjectSubmissions />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
