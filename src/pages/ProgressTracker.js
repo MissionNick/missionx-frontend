121fe61118b85fdc1d1f7de6093a9c8fb80c8436
Brandon Trump <brandontrump101@gmail.com>
2022-07-05 20:30:17 +1200

    created active links, including page not found

---
+++ src/pages/ProgressTracker.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/shared/Navbar";
+import TeacherSidebar from "../components/shared/TeacherSidebar";
+
+export default function ProgressTracker() {
+  return (
+    <div>
+      <Navbar />
+      <TeacherSidebar />
+    </div>
+  );
+}
---
+++ src/pages/ProjectSubmissions.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/shared/Navbar";
+import TeacherSidebar from "../components/shared/TeacherSidebar";
+
+export default function ProjectSubmissions() {
+  return (
+    <div>
+      <Navbar />
+      <TeacherSidebar />
+    </div>
+  );
+}
