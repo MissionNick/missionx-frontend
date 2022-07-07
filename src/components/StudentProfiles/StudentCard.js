17e6d6095c9a47060f0af45bd676b876bc4ec44b
Brandon Trump <brandontrump101@gmail.com>
2022-07-08 01:36:20 +1200

    used composition to make code more reusable

--- src/components/StudentProfiles/StudentCard.js
+++ src/components/StudentProfiles/StudentCard.js
@@ -3,19 +3,15 @@ import styles from "../../components/styles/student-profiles/StudentCard.module.
 
 export default function StudentCard() {
   return (
-    <div className={styles.student_card}>
-      <div className={styles.student_container}>
-        <div className={styles.student_wrapper} id={styles.scroll}>
-          {StudentData.map((item, index) => {
-            return (
-              <div key={index} className={item.cName}>
-                {item.profile_picture}
-                {item.student_name}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div className={styles.student_wrapper} id={styles.scroll}>
+      {StudentData.map((item, index) => {
+        return (
+          <div key={index} className={item.cName}>
+            {item.profile_picture}
+            {item.student_name}
+          </div>
+        );
+      })}
     </div>
   );
 }
