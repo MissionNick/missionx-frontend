c1cdc553386c238f26e4a5e827959725642f38d8
Brandon Trump <brandontrump101@gmail.com>
2022-07-15 03:29:19 +1200

    added progress components to progress page

--- src/pages/ProgressTracker.js
+++ src/pages/ProgressTracker.js
@@ -1,7 +1,9 @@
 import SpreadsheetExport from "../components/ProgressTracker/SpreadsheetExport";
+import StudentProgress from "../components/ProgressTracker/StudentProgress";
 import ContentScrollCard from "../components/shared/ContentScrollCard";
 import Navbar from "../components/shared/Navbar";
 import TeacherSidebar from "../components/shared/TeacherSidebar";
+import SecondaryFooter from "../components/shared/SecondaryFooter";
 
 export default function ProgressTracker() {
   return (
@@ -10,7 +12,9 @@ export default function ProgressTracker() {
       <TeacherSidebar />
       <ContentScrollCard>
         <SpreadsheetExport />
+        <StudentProgress />
       </ContentScrollCard>
+      <SecondaryFooter />
     </div>
   );
 }
