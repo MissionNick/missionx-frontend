5aabf7d9b770ad917b4da088c4cf25cde3bec588
Brandon Trump <brandontrump101@gmail.com>
2022-07-15 03:27:51 +1200

    created components to display student progress

--- src/components/ProgressTracker/SpreadsheetExport.js
+++ src/components/ProgressTracker/SpreadsheetExport.js
@@ -1,11 +1,15 @@
 import styles from "../styles/ProgressTracker/ProgressTracker.module.css";
+import exportIcon from "../../assets/images/export.png";
 
 export default function SpreadsheetExport() {
   return (
     <div>
       <div className={styles.exportContainer}>
         <h1 className={styles.title}>Beginner Course</h1>
-        <h2 className={styles.exportTitle}>Export As Spreadsheet</h2>
+        <div className={styles.exportRight}>
+          <img src={exportIcon} alt="export-icon" />
+          <h2 className={styles.exportTitle}>Export As Spreadsheet</h2>
+        </div>
       </div>
     </div>
   );
--- src/components/ProgressTracker/StudentProgress.js
+++ src/components/ProgressTracker/StudentProgress.js
@@ -1,30 +1,48 @@
 import styles from "../styles/ProgressTracker/ProgressTracker.module.css";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function StudentProgress() {
-  //   const [studentList, setStudentList] = useState([]);
+  const [progressList, setProgressList] = useState([]);
 
-  //   useEffect(() => {
-  //     axios.get("http://localhost:4000/api/students").then((response) => {
-  //       setStudentList(response.data);
-  //     });
-  //   }, []);
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/progress").then((response) => {
+      setProgressList(response.data);
+    });
+  }, []);
 
   return (
-    <div className={styles.progressContainer}>
-      {/* {studentList.map((val) => {
+    <>
+      {progressList.map((val) => {
         return (
-          <>
-            <div className={styles.studentInfo}>
-              <h2 className={styles.studentName}>{val.Name}</h2>
-              <p className={styles.studentProgress}>
-                {val.ProjectID}/15 Projects Completed
-              </p>
+          <div className={styles.progressContainer}>
+            <div className={styles.progressContainerLeft}>
+              <div className={styles.studentInfo}>
+                <h2 className={styles.studentName}>{val.Name}</h2>
+                <p className={styles.studentProgress}>
+                  {val.Total}/15 Projects Completed
+                </p>
+              </div>
             </div>
-            <div className={styles.progressGrid}> </div>
-          </>
+            <div className={styles.progressContainerRight}>
+              <p className={styles.progressBtn}>1</p>
+              <p className={styles.progressBtn}>2</p>
+              <p className={styles.progressBtn}>3</p>
+              <p className={styles.progressBtn}>4</p>
+              <p className={styles.progressBtn}>5</p>
+              <p className={styles.progressBtn}>6</p>
+              <p className={styles.progressBtn}>7</p>
+              <p className={styles.progressBtn}>8</p>
+              <p className={styles.progressBtn}>9</p>
+              <p className={styles.progressBtn}>10</p>
+              <p className={styles.progressBtn}>11</p>
+              <p className={styles.progressBtn}>12</p>
+              <p className={styles.progressBtn}>13</p>
+              <p className={styles.progressBtn}>14</p>
+              <p className={styles.progressBtn}>15</p>
+            </div>
+          </div>
         );
-      })} */}
-    </div>
+      })}
+    </>
   );
 }
