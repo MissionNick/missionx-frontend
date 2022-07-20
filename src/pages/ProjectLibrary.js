306301b50af60fbb3e675b66815c5866a51bd463
Brandon Trump <brandontrump101@gmail.com>
2022-07-20 19:16:31 +1200

    quick fix to start app

--- src/pages/ProjectLibrary.js
+++ src/pages/ProjectLibrary.js
@@ -1,24 +1,19 @@
-import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorBoundary } from "react-error-boundary";
 
-import Footer from "../components/shared/Footer"
-import Navbar from "../components/shared/Navbar"
-import Home from "../components/projectlibrary/Home"
-import ScrollToTop from "../components/shared/ScrollToTop"
-
-
+import Footer from "../components/shared/Footer";
+import Navbar from "../components/shared/Navbar";
+import Home from "../components/ProjectLibrary/Home";
+import ScrollToTop from "../components/shared/ScrollToTop";
 
 function ProjectLibrary() {
-
-    
-    
-    return (
-        <ErrorBoundary>
-            <ScrollToTop />
-            <Navbar />
-            <Home />
-            <Footer />           
-        </ErrorBoundary>
-    )
+  return (
+    <ErrorBoundary>
+      <ScrollToTop />
+      <Navbar />
+      <Home />
+      <Footer />
+    </ErrorBoundary>
+  );
 }
 
 export default ProjectLibrary;
