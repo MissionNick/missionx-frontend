6280d1e51df268c08ae5657a6abd59f9af4a173b
MissionNick <the.ardreagh@gmail.com>
2022-07-21 00:57:52 +1200

    CSS fixes - Project, Profile and Navbar

--- src/components/shared/SplitString.js
+++ src/components/shared/SplitString.js
@@ -1,12 +1,8 @@
-export default function SplitString(str,delim) {
+export default function SplitString(str, delim) {
     
-    const str2 = 'project 01 - this is the name';
-    let pos = 0;
-    let matchPos = str2.indexOf('-', pos);
-
-    console.log(`${str2} delimiter ${delim} match pos ${matchPos} `)
-
-    return str.slice(matchPos + 2, str.length);
+    const pos = str.indexOf(delim);
+   
+    return str.slice(pos + 2,str.length);
 
    
 }
